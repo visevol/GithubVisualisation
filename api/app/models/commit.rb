0da0b9492b99db345e6790032665d602010a5555
Jonathan Lalande <lalandej.gg@gmail.com>
2024-10-27 17:01:02 -0400

    fix linter

--- api/app/models/commit.rb
+++ api/app/models/commit.rb
@@ -4,7 +4,7 @@ class Commit < ApplicationRecord
   has_many :source_files, through: :source_file_changes
   serialize :parent_hashes, coder: JSON
 
-  scope :on_changes_ledger, -> { where(for_changes_ledger: true ) }
+  scope :on_changes_ledger, -> { where(for_changes_ledger: true) }
 
   validates :commit_hash,
     presence: true,
